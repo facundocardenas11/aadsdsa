8b6db10a265737ec5db7f508a3ae6a04fb9917a2
Facundo Cardenas <facundocardenas974@gmail.com>
2021-07-19 14:46:19 -0300

    first commit

---
+++ src/main/java/tech/getarrays/employeemanager/exceptions/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package tech.getarrays.employeemanager.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+	
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+
+}
